2abb03e824f2214f9fe30f3e45bb5c65f807582d
Decaloper <88175382+rich1712@users.noreply.github.com>
2026-05-10 18:18:23 -0700

    "add base NFT contract"

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {
+        tokenCounter = 0;
+    }
+}
